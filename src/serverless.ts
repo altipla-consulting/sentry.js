8482cdc5c8b9e6930719423b73ddd22b786735ad
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-08-16 10:10:54 +0200

    Add serverless trpc error function.

---
+++ src/serverless.ts
@@ -0,0 +1,43 @@
+
+import { TRPCError } from '@trpc/server'
+import { getHTTPStatusCodeFromError } from '@trpc/server/http'
+import { logger } from '@altipla/logging'
+import { Toucan } from 'toucan-js'
+import { config } from 'dotenv'
+
+
+interface Context {
+  waitUntil: (promise: Promise<unknown>) => void
+}
+
+config()
+
+export function trpcOnError({ error, request, context }: { error: Error, request: Request, context: Context }) {
+  logger.error(error)
+  if (process.env.SENTRY_DSN && !shouldSilenceError(error)) {
+    let sentry = new Toucan({
+      dsn: process.env.SENTRY_DSN,
+      context,
+      request,
+      requestDataOptions: {
+        allowedHeaders: true,
+        allowedCookies: true,
+        allowedSearchParams: true,
+        allowedIps: true,
+      },
+    })
+    sentry.captureException(error)
+  }
+
+}
+
+function shouldSilenceError(error: Error): boolean {
+  if (error instanceof TRPCError) {
+    let code = getHTTPStatusCodeFromError(error)
+    if (code === 404) {
+      return true
+    }
+  }
+
+  return false
+}
