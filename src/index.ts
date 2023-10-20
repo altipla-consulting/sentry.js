91ad131f75f3c989b7b7e27589b46c05bd744b78
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-10-20 11:33:00 +0200

    Add package name and version to Sentry.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export function expressRequestHandler(): RequestHandler {
   if (process.env.SENTRY_DSN) {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
+      release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
     })
     return Sentry.Handlers.requestHandler()
   } {
